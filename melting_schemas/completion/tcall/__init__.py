e47186aa3e26a391349375ce1fb6b06708670da2
João Severo <joaovbsevero@gmail.com>
2024-06-30 12:52:37 -0300

    Update imports

--- melting_schemas/completion/tcall/__init__.py
+++ melting_schemas/completion/tcall/__init__.py
@@ -1,3 +1,9 @@
+from ..buffered_ml_messages import (
+    ChatMLMessage,
+    ToolCall,
+    ToolCallMLMessage,
+    ToolMLMessage,
+)
 from .params import DynamicParams, StaticParams
 from .requests import HybridTCallRequest, PromptedTCallRequest, RawTCallRequest
 from .responses import TCallResponse
